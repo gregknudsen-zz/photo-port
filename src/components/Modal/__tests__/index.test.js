9f4dfc1b4dd0b548983504232e68fbdde12994fd
Greg <gknudsen@duqlight.com>
2021-03-26 15:40:17 -0400

    add modal tests...still one fail

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+const mockToggleModal = jest.fn();
+
+const currentPhoto = {
+  name: 'Park bench',
+  category: 'landscape',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+  index: 1
+}
+
+afterEach(cleanup)
+
+describe('Modal component', () => {
+  it('renders', () => {
+    render(<Modal
+      currentPhoto={currentPhoto}
+      toggleModal={mockToggleModal}
+    />)
+  })
+  it('matches snapshot DOM node structure', () => {
+  // Arrange the snapshot - declare variables
+  const { asFragment } = render(<Modal />);
+  // Assert the match
+    expect(asFragment()).toMatchSnapshot();
+});
