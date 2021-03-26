827d37835c0c5aa9a527f514d332b34535a2a56f
Greg <gknudsen@duqlight.com>
2021-03-26 14:47:59 -0400

    photo gallery shows with modals now

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,21 +1,17 @@
-import React, { useEffect } from 'react';
-import PhotoList from '../PhotoList';
+import React from "react";
+import PhotoList from '../PhotoList'
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery({ currentCategory }) {
   const { name, description } = currentCategory;
 
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
-
+  
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
       <p>{description}</p>
-      <PhotoList  />
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
-
 export default Gallery;
